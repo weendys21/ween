c91a653d375dba93e4a590dc0509c42722363144
weendys21 <167812412+weendys21@users.noreply.github.com>
2024-05-19 16:53:09 -0600

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,14 @@
+#funcion area
+def area_rectangulo (a,b):
+    area_rec= a*b
+    return area_rec
+#programa Principal
+
+a= 10
+b= 5
+area= area_rectangulo (a,b)
+
+print("El area del rectangulo es: ",area)
+
+
+
